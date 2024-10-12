977a220eef5cbd95dd985e217a8e97b27402ab85
zehra <Zehrag@metropolia.fi>
2024-10-12 11:58:17 +0300

    Lisätty ArrayEquality.java tiedosto

---
+++ ArrayEquality.java
@@ -0,0 +1,32 @@
+public class ArrayEquality {
+
+    public static boolean areArraysEqual(int[] a1, int[] a2) {
+        if (a1 == null || a2 == null) {
+            return a1 == a2;
+        }
+        if (a1.length != a2.length) {
+            return false;
+        }
+        for (int i = 0; i < a1.length; i++) {
+            if (a1[i] != a2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean areMultiDimensionalArraysEqual(int[][] a1, int[][] a2) {
+        if (a1 == null || a2 == null) {
+            return a1 == a2;
+        }
+        if (a1.length != a2.length) {
+            return false;
+        }
+        for (int i = 0; i < a1.length; i++) {
+            if (!areArraysEqual(a1[i], a2[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
